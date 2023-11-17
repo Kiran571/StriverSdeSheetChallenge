4337f03b41348938faf6fc5ce7bc96ed8f8bac8e
Kiran <66951707+Kiran571@users.noreply.github.com>
2023-11-17 13:28:32 +0530

    Create Day3:Arrays_ Code  Testcase Testcase Test Result  74. Search a 2D Matrix.java
    You are given an m x n integer matrix matrix with the following two properties:
    
    Each row is sorted in non-decreasing order.
    The first integer of each row is greater than the last integer of the previous row.
    Given an integer target, return true if target is in matrix or false otherwise.
    
    You must write a solution in O(log(m * n)) time complexity.
    
     Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
    Output: true
    
    TC--O(logmn) m is row , n is col
    SC--O(1)

---
+++ Day3:Arrays_ Code  Testcase Testcase Test Result  74. Search a 2D Matrix.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+       
+        int m=matrix.length;
+        int n=matrix[0].length;
+
+        //apply binary search
+        int low=0,high=m*n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int row=mid/n,col=mid%n;
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) low =mid+1;
+            else high=mid-1;
+
+        }
+        return false;
+        
+    }
+}
